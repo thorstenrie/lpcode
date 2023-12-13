4b1bb649bbaeaf5f6e50734e0687cb11254e6a56
thorstenrie <author@lichtprotoss.com>
2023-12-13 22:42:22 +0100

    Add tests

--- syntax.go
+++ syntax.go
@@ -3,25 +3,37 @@
 // that can be found in the LICENSE file.
 package lpcode
 
+// Import Go standard library packages and tserr
 import (
-	"fmt"
-	"go/format"
+	"fmt"       // fmt
+	"go/format" // format
 
-	"github.com/thorstenrie/tserr"
+	"github.com/thorstenrie/tserr" // tserr
 )
 
+// Code contains the source code as string. The source code is amended by
+// its methods. The source code can be retrieved with String and formatted
+// with Format.
 type Code struct {
-	c string
+	c string // the source code
 }
 
+// NewCode returns a pointer to a new Code instance.
+// The source code in the new Code instance is empty.
 func NewCode() *Code {
+	// Return a new Code instance
 	return &Code{}
 }
 
+// String returns the source code in code as string.
+// It returns an empty string if code is nil.
 func (code *Code) String() string {
+	// Return an empty string if code is nil
 	if code == nil {
+		// Return an empty string
 		return ""
 	}
+	// Return the source code as string
 	return code.c
 }
 
@@ -255,41 +267,65 @@ func (code *Code) KeyedElement(a *KeyedElementArgs) *Code {
 	return code
 }
 
+// Testvars contains the configuration for the generation of test variables with Testvariables
+// A test variable is generated if the corresponding type is not equal to zero.
 type Testvars struct {
 	String, Error, Int, Float int
 }
 
+// Testvariables generates test variables for unit tests. The test variables are generated based
+// on t. A test variable is generated if the corresponding type in t is not equal to zero.
 func (code *Code) Testvariables(t *Testvars) *Code {
+	// Return nil in case code is nil
 	if code == nil {
 		return nil
 	}
+	// Initialize text
 	text := ""
+	// Add a string test variable to text if String is not equal to zero
 	if t.String != 0 {
 		text += "strFoo string = \"foobar\" // test variable type string\n"
 	}
+	// Add an error test variable to text if Error is not equal to zero
 	if t.Error != 0 {
 		text += "errFoo error = fmt.Errorf(strFoo) // test variable type error\n"
 	}
+	// Add an integer test variable to text if Int is not equal to zero
 	if t.Int != 0 {
 		text += "intFoo int64 = 1234 // test variable type int64\n"
 	}
+	// Add a float test variable to text if Float is not equal to zero
 	if t.Float != 0 {
 		text += "floatFoo float64 = 1234 // test variable type float64\n"
 	}
+	// If test variables exist, add them to the variable declaration
 	if text != "" {
 		code.c += "var (\n"
 		code.c += text
 		code.c += ")\n\n"
 	}
+	// Return generated code
 	return code
 }
 
+// Format formats the source code in code in canonical gfmt style.
+// It uses Source from the go/format package. Format returns an error
+// if code is nil or if go/format returns an error.
 func (code *Code) Format() error {
+	// Return an error in cae code is nil
+	if code == nil {
+		return tserr.NilPtr()
+	}
+	// Convert source code into a slice of bytes
 	b := []byte(code.c)
+	// Format the source code using Source from the go/format package
 	o, e := format.Source(b)
+	// Return an error in case Source fails
 	if e != nil {
 		return tserr.Op(&tserr.OpArgs{Op: "format source", Fn: "code", Err: e})
 	}
+	// Convert the formatted source code to string and store it in code
 	code.c = string(o)
+	// Return nil
 	return nil
 }
--- syntax_test.go
+++ syntax_test.go
@@ -3,21 +3,106 @@
 // that can be found in the LICENSE file.
 package lpcode_test
 
+// Import Go standard library package testing as well as lpcode, tserr and tsfio
 import (
-	"fmt"
-	"testing"
+	"testing" // testing
 
-	"github.com/thorstenrie/lpcode"
+	"github.com/thorstenrie/lpcode" // lpcode
+	"github.com/thorstenrie/tserr"  // tserr
+	"github.com/thorstenrie/tsfio"  // tsfio
 )
 
+// TestTestVariables tests generated test variables by Testvariables. The test fails if
+// the generated test variables do not match the contents of the golden file.
 func TestTestVariables(t *testing.T) {
+	// Configure Testvars to generate test variables of types string, error, int and float
 	vars := &lpcode.Testvars{
 		String: 1,
 		Error:  1,
 		Int:    1,
 		Float:  1,
 	}
+	// Retrieve test variables with Testvariables
 	c := lpcode.NewCode().Testvariables(vars)
-	c.Format()
-	fmt.Print(c.String())
+	// Format the retrieved code
+	if e := c.Format(); e != nil {
+		// The test fails if Format returns an error
+		t.Error(e)
+	}
+	// Evaluate the retrieved code with the golden file
+	if e := tsfio.EvalGoldenFile(&tsfio.Testcase{Name: "testvariables", Data: c.String()}); e != nil {
+		// The test fails if the retrieved code does not match the contents of the golden file
+		t.Error(e)
+	}
+}
+
+// TestTestVariablesEmpty tests Testvariables to return an empty string in case
+// no test variables are configured. The test fails if the returned string is
+// not empty.
+func TestTestVariablesEmpty(t *testing.T) {
+	// // Configure Testvars to generate no test variables
+	vars := &lpcode.Testvars{}
+	// Retrieve test variables with Testvariables
+	c := lpcode.NewCode().Testvariables(vars)
+	// The test fails in case the returned string is not empty
+	if c.String() != "" {
+		t.Error(tserr.NotEmpty("code"))
+	}
+}
+
+// TestTestVariablesNil tests Testvariables to return nil in case
+// *Code is nil. The test fails if Testvariables does not return nil.
+func TestTestVariablesNil(t *testing.T) {
+	// Declare c as type *Code and assign nil
+	var c *lpcode.Code = nil
+	// The test fails if Testvariables does not return nil.
+	if n := c.Testvariables(&lpcode.Testvars{}); n != nil {
+		t.Error(tserr.NotNil("Testvariables"))
+	}
+}
+
+// TestFormatNil tests Format to return nil in case
+// *Code is nil. The test fails if Format does not return nil.
+func TestFormatNil(t *testing.T) {
+	// Declare c as type *Code and assign nil
+	var c *lpcode.Code = nil
+	// The test fails if Format does not return nil.
+	if n := c.Format(); n == nil {
+		t.Error(tserr.NilFailed("Format"))
+	}
+}
+
+// TestFormatEmpty tests Format to return nil for an empty code.
+// The test fails if Format returns an error.
+func TestFormatEmpty(t *testing.T) {
+	// Retrieve the return value of Format for an empty code
+	if e := lpcode.NewCode().Format(); e != nil {
+		// The test fails if Format returns an error
+		t.Error(tserr.Op(&tserr.OpArgs{Op: "Format", Fn: "empty source code", Err: e}))
+	}
+}
+
+// TestFormatNoCode tests Format to return an error in case code contains a
+// placeholder text which is not valid Go source code. The test fails if Format returns nil.
+func TestFormatNoCode(t *testing.T) {
+	// Declare testcase loremipsum
+	tc := "loremipsum"
+	// Retrieve golden file path for the testcase
+	fn, err := tsfio.GoldenFilePath(tc)
+	// The test fails if GoldenFilePath returns an error
+	if err != nil {
+		t.Error(tserr.Op(&tserr.OpArgs{Op: "GoldenFilePath", Fn: tc, Err: err}))
+	}
+	// Retrieve the contents of the golden file for the test case
+	li, err := tsfio.ReadFile(fn)
+	// The test fails if ReadFile returns an error
+	if err != nil {
+		t.Error(tserr.Op(&tserr.OpArgs{Op: "ReadFile", Fn: string(fn), Err: err}))
+	}
+	// Retrieve a new Code instance with the contents of the golden file
+	c := lpcode.NewCode().Ident(string(li))
+	// The test fails if Format returns nil
+	if e := c.Format(); e == nil {
+		t.Error(tserr.NilFailed("format code"))
+	}
 }
